e13a22d9cced9c0cf0af346d54f3f14da92f7d97
Jerson Isaac Cruz Albarran <isaac.xpx@gmail.com>
2018-10-04 18:27:37 -0500

    Programa que calcule el maximo comun divisor de dos numeros
    Estructura de datos

---
+++ ad.py
@@ -0,0 +1,11 @@
+
+def mcd (a,b):
+    if b==0:
+        return a
+    else:
+        return mcd(b,a%b)
+
+num1 = int(input("Primer numero: "))
+num2 = int(input("Segundo numero: "))
+
+print("El maximo común divisor es ", mcd(num1,num2))
